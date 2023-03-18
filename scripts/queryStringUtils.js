936a1f9e2946c647da053556f4515bb5a8b884e7
color-task <89162155+color-task@users.noreply.github.com>
2023-03-18 11:26:50 -0400

    Update queryStringUtils.js
    updating prolific vars

--- scripts/queryStringUtils.js
+++ scripts/queryStringUtils.js
@@ -1,10 +1,10 @@
 function saveQueryStringParams() {
 	const searchParams = (new URL(document.location)).searchParams;
-	const prolificPid = searchParams.get('prolificPid');
-	const studyId = searchParams.get('studyId');
-	const sessionId = searchParams.get('sessionId');
+	const PROLIFIC_PID = searchParams.get('PROLIFIC_PID');
+	const STUDY_ID = searchParams.get('STUDY_ID');
+	const SESSION_ID = searchParams.get('SESSION_ID');
 
-	localStorage.setItem('color-task-prolificPid', prolificPid);
-	localStorage.setItem('color-task-studyId', studyId);
-	localStorage.setItem('color-task-sessionId', sessionId);
+	localStorage.setItem('color-task-PROLIFIC_PID', PROLIFIC_PID);
+	localStorage.setItem('color-task-STUDY_ID', STUDY_ID);
+	localStorage.setItem('color-task-SESSION_ID', SESSION_ID);
 }
